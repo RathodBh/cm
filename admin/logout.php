53df58a85e9e2650e9e4589a6eac6b82c3bd3bda
Rathod Bhavesh <100901679+RathodBh@users.noreply.github.com>
2022-09-10 23:10:11 +0530

    Delete admin directory

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include("../include/config.php");
-$_SESSION['alogin'] == "";
-session_unset();
-?>
-<script language="javascript">
-    document.location = "admin-login.php";
-</script>
